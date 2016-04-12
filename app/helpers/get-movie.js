eb9e5839d3e72dc810b2d13816966dde0628b460
Jeremy Castillo <jeremy.castillo14@utexas.edu>
2016-04-12 00:16:31 -0500

    display movies with panels. dont have home.hbs anymore. application hbs has navbar and footer (since they are on every page).

--- app/helpers/get-movie.js
+++ app/helpers/get-movie.js
@@ -1,17 +1,17 @@
-import Ember from 'ember';
+// import Ember from 'ember';
 
-export function getMovie(params/*, hash*/) {
-  var movie;
-  $.ajax({
-	  dataType: "json",
-	  url: 'http://localhost:8080/v1/movies/56f4a2ddd2e161b93b0a49d5',
-	  async: false,
-	  success: function(data) {
-	  	movie = data;
-	  }
-	});
-  console.log(Ember.$('<div>'+movie.title+'</div>'));
-  return Ember.String.htmlSafe('<div>'+movie.title+'<br>'+movie.genre+'</div>');
-}
+// export function getMovie(params/*, hash*/) {
+//   var movie;
+//   $.ajax({
+// 	  dataType: "json",
+// 	  url: 'http://localhost:8080/v1/movies//search/:The Dark Knight',
+// 	  async: false,
+// 	  success: function(data) {
+// 	  	movie = data;
+// 	  }
+// 	});
+//   console.log(Ember.$('<div>'+movie.title+'</div>'));
+//   return Ember.String.htmlSafe('<div>'+movie.title+'<br>'+movie.genre+'</div>');
+// }
 
-export default Ember.Helper.helper(getMovie);
+// export default Ember.Helper.helper(getMovie);
