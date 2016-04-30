257cae618eabbfea2aa056351c6fb3667997071d
Cesar Gonzalez <2cesargonzalez@gmail.com>
2016-04-30 12:52:05 -0500

    took out console.log()'s and commented out code

--- app/routes/movies/index.js
+++ app/routes/movies/index.js
@@ -2,20 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    console.log('Im in index.js');
-    console.log(params.movie_id);
-//    var movie = {};
-//    const movie_id = params.movie_id;
-//    Ember.$.ajax({
-//        dataType: "json",
-//        url: '/v1/movies/'+movie_id,
-//    async: false,
-//        success: function(data){
-//            console.log(data);
-//            movie = data;
-//        }
-//    });
-//    return movie;
 		const query = params.movie_id;
 		if(query) {
 		  return new Ember.RSVP.Promise(function(resolve, reject) {
@@ -24,9 +10,7 @@ export default Ember.Route.extend({
           resolve
         ).fail(reject);
       }).then(function(data) {
-        console.log(data);
         params['data'] = data;
-        console.log(params);
         return params;
       });
 		}
