ff6f23e256f639c6408090085e655a9b9c084de2
jeremycastillo <jeremycastillo@users.noreply.github.com>
2016-04-30 14:52:38 -0500

    Login and Register and Style (#10)
    * my way of getting user from database and displaying name in top right corner.
    
    * added ember cli sass and code for the look of the site.
    
    * style code. login now shows username in top right hand corner after login authentication.
    
    * fixed catching of unauthorized (not logged in) user error. everything redirects to login page when not logged in

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
+import Cookie from 'moviematcher/utils/cookies';
+import RestUtils from 'moviematcher/utils/rest';
 
 export default Ember.Route.extend({
+	model() {
+	 	const cookie = Cookie.create({name: 'session'}).load();
+	 	if(cookie === "") {
+	 		this.transitionTo('login');
+	 	}
+	 	else {
+			return new RestUtils().get('v1/login', {
+				headers: {
+					"Authorization": cookie
+				}
+			})
+			.then((value) => {
+				return value;
+			});
+		}
+	}
 });
