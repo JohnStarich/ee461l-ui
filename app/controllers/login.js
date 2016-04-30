d71c902046052e861489274e0c7aaa4769b9b064
Jeremy Castillo <jeremy.castillo14@utexas.edu>
2016-04-30 13:16:26 -0500

    style code. login now shows username in top right hand corner after login authentication.

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -7,6 +7,7 @@ export default Ember.Controller.extend({
 
   actions: {
 		userLogin(username, password) {
+      const loginThis = this;
 		  const sessionDurationSeconds = 2 * 60 * 60; // 2 hours
 			return RestUtils.post(undefined, '/v1/users/login', {data: {username, password}})
         .then((value) => {
