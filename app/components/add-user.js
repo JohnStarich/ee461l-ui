dfdc7191a93d235ebbc1253ea754806d080f2ead
Jeremy Castillo <jeremy.castillo14@utexas.edu>
2016-05-04 18:23:21 -0500

    made changes for pr

--- app/components/add-user.js
+++ app/components/add-user.js
@@ -4,6 +4,7 @@ import RestUtils from 'moviematcher/utils/rest';
 export default Ember.Component.extend({
 	actions: {
 		addFriend(params) {
+			 if(! params) { throw new Error("No friend to add"); }
 			 var newFriend_id = params;
 			 return RestUtils.post(this, 'v1/friends', {data: {newFriend_id: newFriend_id}})
 				.then((value) => {
--- app/components/user-info.js
+++ app/components/user-info.js
@@ -4,6 +4,7 @@ import RestUtils from 'moviematcher/utils/rest';
 export default Ember.Component.extend({
 	actions: {
 		deleteFriend(params) {
+		  if(! params) { throw new Error("No friend to delete"); }
 		  var delFriend_id = params;
 		  return RestUtils.delete(this, `v1/friends/${delFriend_id}`)
 		  	.then( (value) => {
