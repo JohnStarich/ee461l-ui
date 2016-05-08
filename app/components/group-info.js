995484accd8a73baefe413a6bf2dbbf55ac1c4ae
John Starich <john.starich@thirdship.com>
2016-05-08 17:21:48 -0500

    Add removeUser feature

--- app/components/group-info.js
+++ app/components/group-info.js
@@ -15,6 +15,17 @@ export default Ember.Component.extend({
 					alert(value);
 				});
 		},
+
+		removeUser(member_id) {
+			const group_id = this.get('group').id;
+			if(! group_id) { throw new Error("No group ID provided."); }
+			if(! member_id) { throw new Error("No member ID provided."); }
+			RestUtils.delete(this, `/v1/groups/${group_id}/${member_id}`).then((value) => {
+				alert(value);
+				const new_group_members = this.get('group.members').filter((member) => member.id !== member_id);
+				this.set('group.members', new_group_members);
+			});
+		},
 	}
 });
 
